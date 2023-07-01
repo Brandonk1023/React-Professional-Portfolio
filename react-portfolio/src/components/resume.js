236f1a275c60c933c06620f74cbba3cf3647c0c9
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-01 14:46:35 -0400

    began adding coding for resume section

---
+++ react-portfolio/src/components/resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const resume = () => {
+    return(
+        <div>
+            <h2>Thanks for taking a look at my resume below.</h2>
+            <p> You can download a PDF copy by clicking <a href='react-portfolio\assets\Resume.pdf'>here</a>
+            </p>
+            <div></div>
+        </div>
+    )
+}
+
+export default resume;
