80c08e78452dcc0d8ca8436f2fef4e2054b22b76
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-01 13:12:26 -0400

    added project file for project data

--- react-portfolio/src/components/aboutMe.js
+++ react-portfolio/src/components/aboutMe.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import headshot from '';
 
 const aboutMe =  () => {
     return (
--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
+import headShot from '';
 const header = () => {
     return (
         <header>
-            <h1>
-                Brandon Kilgore
-            </h1>
+            <h1>Brandon Kilgore </h1>
+            <p>Full Stack Developer</p>
         </header>
     )
 }
