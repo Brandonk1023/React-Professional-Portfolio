246cdd3a3290d60aaa26d4f3f1255bb25949c0dc
Brandon Kilgore <brandonk1023@gmail.com>
2023-06-30 21:39:04 -0400

    added code into footer

--- react-portfolio/src/components/aboutMe.js
+++ react-portfolio/src/components/aboutMe.js
@@ -6,7 +6,7 @@ const aboutMe =  () => {
         <section>
             <div>
                 <h2>About Me</h2>
-                <p>About me content</p>
+                <p>Hi my name is Charles Brandon Kilgore. I'm an Atlanta-based full stack developer. I'm interested in backend technologie, cyber securit, and additive manufacturing technologies.</p>
             </div>
         </section>
     )
--- react-portfolio/src/components/footer.js
+++ react-portfolio/src/components/footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import linkedinLogo from '';
+import githubLogo from '';
 
 const footer = () => {
     return (
         <footer>
             <div>
-
+                <a href='https://www.linkedin.com/in/charles-kilgore-69695b135/'>
+                    <img src={linkedinLogo} alt='LinkedIn Logo'/>
+                </a>
+                <a href='https://github.com/Brandonk1023'>
+                    <img src={githubLogo} alt='GitHub Logo'/>
+                </a>
             </div>
         </footer>
     )
