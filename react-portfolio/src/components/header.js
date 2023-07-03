1ad783224744ed567ffff7497126f0cb63960e23
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-03 16:29:21 -0400

    added head shot link and code into header

--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import headShot from '';
+import headShot from 'react-portfolio/assets/HSSquare.jpg';
 const Header = () => {
     return (
         <header>
+            <img src={headShot} alt='Headshot of Brandon'></img>
             <h1>Brandon Kilgore </h1>
             <p>Full Stack Developer</p>
         </header>
