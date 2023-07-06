6a5f5a50e34648859fdb56a15bc100e71fa41149
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-06 12:09:01 -0400

    fixed download pdf functionality for resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import resume from '../assets/Resume.pdf'
 const Resume = () => {
     return(
         <div>
            {/*  <h2>resume</h2> */}
-            <p> Thanks for your interest in my resume. You can download a PDF copy by clicking <a className='link link-primary' href="src/assets/Resume.pdf" download="Brandon Kilgore Resume.pdf">here</a>.
+            <p> Thanks for your interest in my resume. You can download a PDF copy by clicking <a className='link link-primary' href={resume} download="Brandon Kilgore Resume.pdf">here</a>.
             </p>
             <div></div>
         </div>
