2d582cd9dc2387d8caa4444af2db23c9fd5420e9
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-06 00:08:57 -0400

    added photos to portfolio page

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import Project from './project';
+import FoodWillBeerUsApart from '../assets/projectPhotos/FoodWillBeerUsApart.jpg';
+import ExpressNoteTaker from '../assets/projectPhotos/Express-Note-Taker.jpg';
+import NoSQLAPIscreenshot from '../assets/projectPhotos/NoSQLAPIscreenshot.jpg';
+import ORMAPI from '../assets/projectPhotos/ORM-API.jpg'
+import PotItLikeItsHot from '../assets/projectPhotos/PotItLikeItsHot.jpg';
+import SVGLogoGen from '../assets/projectPhotos/SVGLogoGen.jpg'
+
 
 const Portfolio = () => {
     const [projects] = useState([
@@ -8,42 +15,42 @@ const Portfolio = () => {
                 github: 'https://github.com/Brandonk1023/FoodWillBeerUsApart',
                 link: '',
                 content: 'This is an application that provides the user with an easy to use tool to create food and beer pairings.',
-                image: '',
+                image: FoodWillBeerUsApart,
             },
             {
                 title: "Pot It Like It's Hot",
                 github: 'https://github.com/stbuiemory/2kPerDay',
                 link: '',
                 content: 'This is an application that allows the user to learn about plants and cultivate a digital garden. This app is still currently under construction',
-                image: '',
+                image: PotItLikeItsHot,
             },
             {
                 title: 'NoSQL-Social-Network-API',
                 github: 'https://github.com/Brandonk1023/NoSQL-Social-Network-API/tree/main',
                 link: '',
                 content: 'This is a mock back-end API for a social network, using a NoSQL, MongoDB database.',
-                image: '',
+                image: NoSQLAPIscreenshot,
             },
             {
                 title: 'ORM-Ecommerce-Back-End',
                 github: 'https://github.com/Brandonk1023/ORM-Ecommerce-Back-End',
                 link: '',
                 content: 'This is mock back-end MySQL application for an ecommerce site.',
-                image: '',
+                image: ORMAPI,
             },
             {
                 title: 'Express-Note-Taker',
                 github: 'https://github.com/Brandonk1023/Express-Note-Taker',
                 link: '',
                 content: 'This is a note taking application that a user can use for planning and organization.',
-                image: '',
+                image: ExpressNoteTaker,
             },
             {
                 title: 'OOP-SVG-Logo-Maker',
                 github: 'https://github.com/Brandonk1023/OOP-SVG-Logo-Maker',
                 link: '',
                 content: 'This application is a simple SVG Logo generator',
-                image: '',
+                image: SVGLogoGen,
             },
         ]);
 
--- src/components/project.js
+++ src/components/project.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Project = ({ project }) => {
     return (
         <div className="flex flex-row card w-96 glass justify-around">
-            <img src={project.image} alt={project.title} />
+            <img className="w-40 rounded" src={project.image} alt={project.title} />
             <div class="flex flex-col justify-around">
                 <h2>{project.title}</h2>
                 <div class="flex flex-col justify-around">
