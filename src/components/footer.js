45c203f484423f6f40df199e046c50fd30ee5cd7
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-05 20:44:33 -0400

    added more styling to site.  Added styling to footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,12 +5,12 @@ import githubLogo from '../assets/logos/gitHubFooterLogo.png';
 const Footer = () => {
     return (
         <footer>
-            <div>
+            <div class="bg-gradient-to-r from-violet-500 to-fuchsia-500">
                 <a href='https://www.linkedin.com/in/charles-kilgore-69695b135/'>
-                    <img src={linkedinLogo} alt='LinkedIn Logo'/>
+                    <img className="w-32 rounded"  src={linkedinLogo} alt='LinkedIn Logo'/>
                 </a>
                 <a href='https://github.com/Brandonk1023'>
-                    <img src={githubLogo} alt='GitHub Logo'/>
+                    <img className="w-32 rounded" src={githubLogo} alt='GitHub Logo'/>
                 </a>
             </div>
         </footer>
