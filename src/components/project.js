6a3e35b05b84db1f4987b54a9c3cc4a457179514
Brandon Kilgore <brandonk1023@gmail.com>
2023-07-28 14:53:59 -0400

    removed view project button

--- src/components/project.js
+++ src/components/project.js
@@ -7,9 +7,9 @@ const Project = ({ project }) => {
             <div class="flex flex-col justify-around">
                 <h2 className='text-lg'>{project.title}</h2>
                 <div class="flex flex-col justify-around">
-                    <button className='btn btn-secondary w-48 mb-1'>
+                   {/*  <button className='btn btn-secondary w-48 mb-1'>
                     <a href={project.link} target="_blank" rel="noreferrer" >View Project</a>
-                    </button>
+                    </button> */}
                     <button className='btn btn-accent w-48'>
                     <a href={project.github} target="_blank" rel="noreferrer" >GitHub</a>
                     </button>
